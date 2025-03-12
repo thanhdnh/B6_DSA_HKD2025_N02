134e4be60942627c2d2dd5a34037976df445cb4a
thanhdnh <myhoangthanh@yahoo.com>
2025-03-12 08:26:54 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿public class Node{
+    public object element;
+    public Node link;
+    public Node(object element){
+        this.element = element;
+        this.link = null;
+    }
+}
+public class LinkedList{
+    public Node head;
+    public LinkedList(){
+        head = new Node("Header");
+    }
+    public Node Find(object item){
+        Node current = head;
+        while(current.element != item){
+            current = current.link;
+        }
+        return current;
+    }
+    public void Insert(object newel, object afterel){
+        Node current = Find(afterel);
+        Node newnode = new Node(newel);
+        newnode.link = current.link;
+        current.link = newnode;
+    }
+    public Node FindPrev(object item){
+        Node current = head;
+        while(current.link != null && current.link.element != item){
+            current = current.link;
+        }
+        return current;
+    }
+    public void Remove(object item){
+        Node current = FindPrev(item);
+        if(current.link != null){
+            current.link = current.link.link;
+        }
+    }
+    public void Print(){
+        Node current = head;
+        while(current != null){
+            Console.WriteLine(current.element);
+            current = current.link;
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+        list.Insert("1", "Header");
+        list.Insert("2", "1");
+        list.Insert("3", "2");
+        list.Print();
+    }
+}
